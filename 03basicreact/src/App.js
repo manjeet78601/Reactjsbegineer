3422d596787dc90ce4d69708337db8aa1b79f7c7
manjeet78601 <your-manjeetpathania13@gmail.com>
2025-07-21 17:03:53 +0530

    All folder commited

---
+++ 03basicreact/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const username='thecodeplanner'
+  return (
+    <>
+    <tech/>
+    <h1>Welcome to tech stack {username} </h1>
+<p>test para</p>
+</>
+ );
+}
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+//   );
+// }
+
+export default App;
